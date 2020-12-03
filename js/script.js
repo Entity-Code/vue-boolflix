9682086708c81ab49f65eabdcd573efbcafbd364
Entity-Code <mattia.tummolilllo@gmail.com>
2020-12-03 17:39:42 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -15,10 +15,25 @@ var app = new Vue({
       research: ""
    },
    mounted: function() {
+      // risultati che appaiono appena aperto il sito
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=632b3a13e5a3fa9d76198ee6af3fe116&query=Pink Floyd")
       .then(risposta => {
 
+         this.movies = risposta.data.results;
+         for (var i = 0; i < this.movies.length; i++) {
 
+            // arrotondamento voto
+            let votoDiviso = (this.movies[i].vote_average / 2);
+            var votoArrotondato = (Math.ceil(votoDiviso));
+
+            // modifica dei voti arrotondati
+            this.movies[i].stars = votoArrotondato;
+
+            let lowerLanguage = this.movies[i].original_language;
+            let upperLanguage = lowerLanguage.toUpperCase(lowerLanguage);
+
+            this.movies[i].upperLanguage = upperLanguage;
+          }
       });
 
    },
